9b13dc8518c1e1eb1a465ce9ee0409ff516b71ca
VuXfi <x@42.pub>
2025-08-28 11:22:29 +0700

    Revert "fix: Update mime type retrieval to use mime.lookup for consistency"
    This reverts commit d45d18390e0332c68e494de1fe5f2dc5067c2532.

--- server/db/migrations/20250820144730_track_storage_usage.js
+++ server/db/migrations/20250820144730_track_storage_usage.js
@@ -87,7 +87,7 @@ exports.up = async (knex) => {
       id: avatar.uploadedFileId,
       type: 'userAvatar',
       referencesTotal: 1,
-      mimeType: mime.lookup(avatar.extension),
+      mimeType: mime.getType(avatar.extension),
       size: avatar.size,
     })),
   );
@@ -100,7 +100,7 @@ exports.up = async (knex) => {
       id: backgroundImage.uploaded_file_id,
       type: 'backgroundImage',
       referencesTotal: 1,
-      mimeType: mime.lookup(backgroundImage.extension),
+      mimeType: mime.getType(backgroundImage.extension),
       size: backgroundImage.size,
       createdAt: backgroundImage.created_at,
     })),
